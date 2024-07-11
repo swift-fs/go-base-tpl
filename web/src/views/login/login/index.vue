f909f184e3df3493210622f960b3f438a5881588
mh-swift <zuojiadazhongma@gmail.com>
2024-07-11 16:53:44 +0800

    feat: 去除手机号登录

--- web/src/views/login/login/index.vue
+++ web/src/views/login/login/index.vue
@@ -1,12 +1,13 @@
 <template>
-  <n-tabs type="segment" justify-content="space-evenly">
+  <Form @update-active-module="updateActiveModule" mode="account" />
+  <!-- <n-tabs type="segment" justify-content="space-evenly">
     <n-tab-pane name="account" tab="账号登录">
       <Form @updateActiveModule="updateActiveModule" mode="account" />
     </n-tab-pane>
     <n-tab-pane name="mobile" tab="手机号登录">
       <Form @updateActiveModule="updateActiveModule" mode="mobile" />
     </n-tab-pane>
-  </n-tabs>
+  </n-tabs> -->
 </template>
 
 <script lang="ts" setup>
